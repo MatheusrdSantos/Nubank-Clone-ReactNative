88680512f156a3f7278d2c6d4b8a508768e00ff9
Matheus Ricardo dos Santos <matheus.r.d.s10@hotmail.com>
2019-07-20 20:25:37 -0300

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+const App = () => {
+  return (
+      <View style={{flex:1, backgroundColor: '#b707b7', justifyContent: 'center', alignItems:'center'}}>
+          <Text style={{color: '#fff'}}>New Nubank app</Text>
+      </View>
+  );
+};
+
+export default App;
