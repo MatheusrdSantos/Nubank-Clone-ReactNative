ead07e6feaf5aa43a078fbd3d346934fe8a5ac29
Matheus Ricardo dos Santos <matheus.r.d.s10@hotmail.com>
2019-07-22 22:03:28 -0300

    Implements bottom buttons style

--- app/components/Tabs/index.js
+++ app/components/Tabs/index.js
@@ -2,16 +2,45 @@ import React from 'react';
 
 import { View } from 'react-native';
 
-import { Container, Tab } from './styles';
-
+import { Container, Tab, TabDescription, Icon } from './styles';
+import indicarAmigo from '../../assets/indicar-amigo.png';
+import cobrar from '../../assets/cobrar.png';
+import emprestimos from '../../assets/emprestimos.png';
 const Tabs = () => (
     <Container>
-        <Tab></Tab>
-        <Tab></Tab>
-        <Tab></Tab>
-        <Tab></Tab>
-        <Tab></Tab>
-        <Tab></Tab>
+        <Tab>
+            <Icon source={indicarAmigo}/>
+            <TabDescription>Indicar amigos</TabDescription>
+        </Tab>
+        <Tab>
+            <Icon source={cobrar}/>
+            <TabDescription>Cobrar</TabDescription>
+        </Tab>
+        <Tab>
+            <Icon source={emprestimos}/>
+            <TabDescription>Empréstimos</TabDescription>
+        </Tab>
+        <Tab>
+            <TabDescription>Depositar</TabDescription>
+        </Tab>
+        <Tab>
+            <TabDescription>Transferir</TabDescription>
+        </Tab>
+        <Tab>
+            <TabDescription>Ajustar limite</TabDescription>
+        </Tab>
+        <Tab>
+            <TabDescription>Pagar</TabDescription>
+        </Tab>
+        <Tab>
+            <TabDescription>Bloquer cartão</TabDescription>
+        </Tab>
+        <Tab>
+            <TabDescription>Cartão virtual</TabDescription>
+        </Tab>
+        <Tab>
+            <TabDescription>Organizar atalhos</TabDescription>
+        </Tab>
     </Container>
 );
 
--- app/components/Tabs/styles.js
+++ app/components/Tabs/styles.js
@@ -13,11 +13,19 @@ export const Container = styled.ScrollView.attrs({
 export const Tab = styled.View`
     height: 100px;
     width: 100px;
-    background-color: rgba(255, 255, 255, 0.2);
+    background-color: rgba(255, 255, 255, 0.1);
     border-radius: 3px;
     margin-left: 10px;
     margin-bottom: 18px;
     padding: 10px;
     justify-content: space-between;
     align-self: flex-end;
+`
+
+export const TabDescription = styled.Text`
+    color: #fff;
+    font-size: 13px;
+`
+
+export const Icon = styled.Image`
 `
