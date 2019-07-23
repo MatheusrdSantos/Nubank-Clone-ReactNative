dfc2a96e8f1ddbde4062f17ebb33d9c20cf9d493
Matheus Ricardo dos Santos <matheus.r.d.s10@hotmail.com>
2019-07-23 20:45:51 -0300

    Implements all bottom icons created

--- app/components/Tabs/index.js
+++ app/components/Tabs/index.js
@@ -6,6 +6,13 @@ import { Container, Tab, TabDescription, Icon } from './styles';
 import indicarAmigo from '../../assets/indicar-amigo.png';
 import cobrar from '../../assets/cobrar.png';
 import emprestimos from '../../assets/emprestimos.png';
+import depositar from '../../assets/depositar.png';
+import transferir from '../../assets/transferir.png';
+import ajustarLimite from '../../assets/ajustar-limite.png';
+import pagar from '../../assets/pagar.png';
+import bloquearCartao from '../../assets/bloquear-cartao.png';
+import cartaoVirtual from '../../assets/cartao-virtual.png';
+import organizarAtalhos from '../../assets/organizar-atalhos.png';
 const Tabs = () => (
     <Container>
         <Tab>
@@ -21,24 +28,31 @@ const Tabs = () => (
             <TabDescription>Empréstimos</TabDescription>
         </Tab>
         <Tab>
+            <Icon source={depositar}/>
             <TabDescription>Depositar</TabDescription>
         </Tab>
         <Tab>
+            <Icon source={transferir}/>
             <TabDescription>Transferir</TabDescription>
         </Tab>
         <Tab>
+            <Icon source={ajustarLimite}/>
             <TabDescription>Ajustar limite</TabDescription>
         </Tab>
         <Tab>
+            <Icon source={pagar}/>
             <TabDescription>Pagar</TabDescription>
         </Tab>
         <Tab>
+            <Icon source={bloquearCartao}/>
             <TabDescription>Bloquer cartão</TabDescription>
         </Tab>
         <Tab>
+            <Icon source={cartaoVirtual}/>
             <TabDescription>Cartão virtual</TabDescription>
         </Tab>
         <Tab>
+            <Icon source={organizarAtalhos}/>
             <TabDescription>Organizar atalhos</TabDescription>
         </Tab>
     </Container>
